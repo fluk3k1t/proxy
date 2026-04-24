bd5f9a87e9bf3cb0e30f4eec3b19895e52639d1f
fluk3k1t <flukekit0316@gmail.com>
2026-04-24 14:54:34 +0900

    bu

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,61 @@
-use std::fs;
+use std::{fs, time::Duration};
 
+use bytes::BytesMut;
 use rcgen::{
     CertificateParams, CertifiedKey, Issuer, KeyPair, SigningKey, generate_simple_self_signed,
+};
+use tokio::{
+    io::{AsyncReadExt, BufWriter},
+    net::{TcpListener, TcpStream},
+    time::{error::Elapsed, timeout},
 };
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client_key_pair = KeyPair::generate().unwrap();
+    let mut listener = TcpListener::bind("127.0.0.1:8000").await?;
 
-    let client_cert_param = CertificateParams::new(vec!["example.com".to_string()]).unwrap();
-
-    let ca_pem_str = fs::read_to_string("ore_ca.cert").unwrap();
-    let ca_signing_key = fs::read_to_string("ore_ca.key").unwrap();
-
-    let ca_issuer =
-        Issuer::from_ca_cert_pem(&ca_pem_str, KeyPair::from_pem(&ca_signing_key).unwrap()).unwrap();
-    // println!("{:?}", ca_issuer);
-
-    let cert = client_cert_param
-        .signed_by(&client_key_pair, &ca_issuer)
-        .unwrap();
-
-    println!("{}", cert.pem());
+    match listener.accept().await {
+        Ok((socket, addr)) => {
+            tokio::spawn(handle_connection(socket));
+        }
+        Err(e) => println!("couldn't get client: {:?}", e),
+    }
 
     Ok(())
+}
+
+pub struct Connection {
+    // stream: BufWriter<TcpStream>,
+    stream: TcpStream,
+    // buffer: BytesM
+    buffer: BytesMut,
+}
+
+impl Connection {
+    pub async fn read_http_with_timeout(&mut self, duration: Duration) {
+        // match timeout(
+        //     Duration::from_millis(1000),
+        //     self.stream.read(&mut self.buffer),
+        // )
+        // .await?
+        // {
+        //     Ok(n) => {}
+        //     Err(e) => {}
+        // }
+    }
+
+    pub async fn read_http(&mut self) {
+        loop {
+            // self.stream.read_buf(&mut self.buffer).await;
+            // self.stream.t
+            // self.stream.readable().await;
+            // self.stream.try_read_buf()
+        }
+    }
+}
+
+async fn handle_connection(mut stream: TcpStream) -> Result<(), Elapsed> {
+    let mut tmp = [0; 1024];
+
+    loop {}
 }
