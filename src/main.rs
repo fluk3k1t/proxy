86f06ff37c709f5d0ac2d67afb51238755fbc553
fluk3k1t <flukekit0316@gmail.com>
2026-04-26 14:46:57 +0900

    impl http proxy

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,77 @@
-use std::{fs, time::Duration};
+use std::{
+    collections::HashMap,
+    fs,
+    io::{Error, ErrorKind},
+    iter::Map,
+    time::Duration,
+};
 
-use bytes::BytesMut;
+use async_from::{AsyncFrom, AsyncInto, async_trait};
+use bytes::{Bytes, BytesMut};
+// use futures_core::Stream;
+// use futures_util::StreamExt;
+use httparse::{EMPTY_HEADER, Request, Status};
 use rcgen::{
     CertificateParams, CertifiedKey, Issuer, KeyPair, SigningKey, generate_simple_self_signed,
 };
+use reqwest::Response;
 use tokio::{
-    io::{AsyncReadExt, BufWriter},
+    io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, BufWriter},
     net::{TcpListener, TcpStream},
     time::{error::Elapsed, timeout},
 };
+use tokio_stream::StreamExt;
+use tokio_util::codec::{Decoder, Framed, FramedRead};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut listener = TcpListener::bind("127.0.0.1:8000").await?;
 
-    match listener.accept().await {
-        Ok((socket, addr)) => {
-            tokio::spawn(handle_connection(socket));
+    loop {
+        match listener.accept().await {
+            Ok((socket, addr)) => {
+                let mut conn = Connection::new(socket);
+                tokio::spawn(async move { conn.handle_connection().await });
+            }
+            Err(e) => println!("couldn't get client: {:?}", e),
         }
-        Err(e) => println!("couldn't get client: {:?}", e),
     }
 
     Ok(())
+}
+
+pub struct RawResponse {
+    resp: Response,
+}
+
+impl From<Response> for RawResponse {
+    fn from(value: Response) -> Self {
+        Self { resp: value }
+    }
+}
+
+#[async_trait]
+impl AsyncFrom<RawResponse> for Bytes {
+    async fn async_from(raw: RawResponse) -> Self {
+        let status = raw.resp.status();
+        let headers = raw.resp.headers().clone();
+        let body = raw.resp.text().await.unwrap();
+
+        let mut presp = format!("HTTP/1.1 {}\n", status);
+        for (n, v) in headers.iter() {
+            let v = String::from_utf8_lossy(v.as_bytes());
+            presp.push_str(&format!("{}: {}\r\n", n, v));
+        }
+        presp.push_str("\r\n");
+
+        if headers.iter().any(|(h, v)| h == "Transfer-Encoding") {
+            presp.push_str(&format!("{:x}\r\n{}", body.len(), body));
+        } else {
+            presp.push_str(body.as_str());
+        }
+
+        Bytes::from(presp)
+    }
 }
 
 pub struct Connection {
@@ -32,30 +82,101 @@ pub struct Connection {
 }
 
 impl Connection {
-    pub async fn read_http_with_timeout(&mut self, duration: Duration) {
-        // match timeout(
-        //     Duration::from_millis(1000),
-        //     self.stream.read(&mut self.buffer),
-        // )
-        // .await?
-        // {
-        //     Ok(n) => {}
-        //     Err(e) => {}
-        // }
+    pub fn new(stream: TcpStream) -> Self {
+        Connection {
+            stream,
+            buffer: BytesMut::new(),
+        }
     }
 
-    pub async fn read_http(&mut self) {
-        loop {
-            // self.stream.read_buf(&mut self.buffer).await;
-            // self.stream.t
-            // self.stream.readable().await;
-            // self.stream.try_read_buf()
+    async fn handle_connection(&mut self) -> Result<(), Error> {
+        let mut packet_reader = Framed::new(&mut self.stream, HttpDecoder::default());
+
+        while let Some(r) = packet_reader.next().await {
+            match r {
+                Ok(packet) => {
+                    self.handle_packet(packet).await;
+                    return Ok(());
+                }
+                Err(e) => return Err(e),
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn handle_packet(&mut self, packet: Packet) {
+        if let Some(method) = packet.method {
+            let host = packet.headers.get("Host").unwrap();
+
+            match &method[..] {
+                "GET" => {
+                    let resp = reqwest::get(format!("http://{}", host)).await.unwrap();
+                    let resp: RawResponse = resp.into();
+                    let raw_packet: Bytes = resp.async_into().await;
+
+                    self.stream.write_all(&raw_packet).await.unwrap();
+                }
+                "CONNECT" => {}
+                _ => {}
+            }
+        } else {
+            panic!("method does not exist!");
         }
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) -> Result<(), Elapsed> {
-    let mut tmp = [0; 1024];
+#[derive(Debug)]
+pub struct Packet {
+    headers: HashMap<String, String>,
+    method: Option<String>,
+}
 
-    loop {}
+pub struct HttpDecoder {}
+
+impl Default for HttpDecoder {
+    fn default() -> Self {
+        HttpDecoder {}
+    }
+}
+
+impl Decoder for HttpDecoder {
+    type Error = std::io::Error;
+    type Item = Packet;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        // プロキシに関連するHTTPメソッドはボディを持たないはずなのでヘッダーのみを読む
+        // ボディが存在して、かつパケットが分割されて送られてくると次回以降のパケットの読み込みに支障をきたす可能性はある
+        if src.windows(4).any(|w| w == b"\r\n\r\n") {
+            let mut headers = [EMPTY_HEADER; 64];
+            let mut req = Request::new(&mut headers);
+
+            match req
+                .parse(src)
+                .map_err(|_| std::io::Error::new(ErrorKind::Other, "oh no!"))?
+            {
+                Status::Partial => Ok(None),
+                Status::Complete(n) => {
+                    let headers = req
+                        .headers
+                        .iter()
+                        .map(|h| {
+                            (
+                                h.name.to_owned(),
+                                String::from_utf8_lossy(h.value).to_string(),
+                            )
+                        })
+                        .collect();
+
+                    let method = req.method.map(String::from);
+
+                    src.clear();
+
+                    Ok(Some(Packet { headers, method }))
+                }
+            }
+        } else {
+            Ok(None)
+        }
+    }
 }
